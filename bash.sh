cbba75b905d6174dbc2b6535d0f59c0d6075d931
Jieun <kyn07077@gmail.com>
2020-03-11 00:27:06 +0900

    Initial commit

---
+++ bash.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+template_dir="$(cd "$(dirname "$0")" && pwd -P)/template"
+
+project_name=${1}
+
+npx create-react-app --template typescript "$project_name"
+echo "eslint + prettier setting"
+
+cd "$project_name" || exit 1
+yarn add eslint @typescript-eslint/eslint-plugin @typescript-eslint/parser prettier eslint-config-prettier eslint-plugin-prettier eslint-config-airbnb
+rm -rf src
+
+rsync -r "$template_dir/" .
